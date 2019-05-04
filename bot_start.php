8f8f6ad7b5c9565082837794970af3a3c9b9b466
KolFoxy <dekolosenko@gmail.com>
2019-05-04 19:57:37 +0300

    Disabled presense cache

--- bot_start.php
+++ bot_start.php
@@ -5,7 +5,8 @@ $config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);
 $loop = \React\EventLoop\Factory::create();
 $client = new \CharlotteDunois\Yasmin\Client([
     'ws.disabledEvents' => ['TYPING_START'],
-    'shardCount' => 4
+    'shardCount' => 4,
+    'presenceCache' => false,
 ], $loop);
 $handler = new \Ancestor\CommandHandler\CommandHandler($client, $config['prefix']);
 $handler->registerCommands(glob(__DIR__ . '/data/bot_commands/*.php'));
