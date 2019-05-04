99a91490bf9a5e8e56e94a98a6572773f742f061
KolFoxy <dekolosenko@gmail.com>
2019-05-04 20:24:19 +0300

    Enabled presence cache back.

--- bot_start.php
+++ bot_start.php
@@ -5,7 +5,6 @@ $config = json_decode(file_get_contents(__DIR__ . '/config.json'), true);
 $loop = \React\EventLoop\Factory::create();
 $client = new \CharlotteDunois\Yasmin\Client([
     'ws.disabledEvents' => ['TYPING_START'],
-    'presenceCache' => false,
 ], $loop);
 $handler = new \Ancestor\CommandHandler\CommandHandler($client, $config['prefix']);
 $handler->registerCommands(glob(__DIR__ . '/data/bot_commands/*.php'));
