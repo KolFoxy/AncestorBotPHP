6e59076200fb0ea081b59e874592dfc13b8ff186
KolFoxy <dekolosenko@gmail.com>
2019-05-16 16:28:25 +0300

    Added writings.json, containing some basic [F$@!ING_BOOKS] data

--- ancestor/Commands/Read.php
+++ ancestor/Commands/Read.php
@@ -15,7 +15,7 @@ class Read extends Command {
     function __construct(CommandHandler $handler) {
         parent::__construct($handler, 'read',
             'You encounter a peace of writing! The consequences can be unforeseen...',
-            ['book', 'fuckbooks']);
+            ['book', 'fuckbooks', 'knowledge']);
     }
 
     function run(\CharlotteDunois\Yasmin\Models\Message $message, array $args) {
